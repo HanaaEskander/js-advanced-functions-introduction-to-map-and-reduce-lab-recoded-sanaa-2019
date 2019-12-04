396446d7bb2acffb1f8990fda20224e817d877ab
Hana'a Eskander <hanaa.eskander95@gmail.com>
2019-12-04 08:28:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,53 +1,54 @@
 // Your code here
 function mapToNegativize(sourceArray){
-  let newArr = [];
-  for (const item of arr) {
-    newArr.push(item * -1);
-  }
-  return newArr;
-}
-
-function mapToNoChange(sourceArray){
-  let newArr = arr;
-  return newArr;
-}
-
-function mapToDouble(sourceArray){
-  let newArr = [];
-  for (const item of arr) {
-    newArr.push(item * 2);
-  }
-  return newArr;
-}
-
-function mapToSquare(sourceArray) {
-  let newArr = [];
-  for (const item of arr) {
-    newArr.push(item * item);
-  }
-  return newArr;
-}
-
-function reduceToTotal(sourceArray, startingPoint=0){
-  for (const item of sourceArray) {
-    startingPoint = startingPoint + item;
-  }
-  return startingPoint;
-}
-
-function reduceToAllTrue(sourceArray){
-  for (const item of sourceArray) {
-    if(item == false){
-      return false;
+    let myArray = [];
+    for (let item of sourceArray) {
+      myArray.push(item * -1);
     }
+    return myArray;
   }
-  return true;
-}
-
-function reduceToAnyTrue(sourceArray){
-  for (const item of sourceArray) {
-    if(item == true){
-      return true;
+  
+  function mapToNoChange(sourceArray){
+    let myArray = sourceArray;
+    return myArray;
+  }
+  
+  function mapToDouble(sourceArray){
+    let myArray = [];
+    for (let item of sourceArray) {
+      myArray.push(item * 2);
     }
+    return myArray;
   }
-  return
+  
+  function mapToSquare(sourceArray) {
+    let myArray = [];
+    for (let item of sourceArray) {
+      myArray.push(item * item);
+    }
+    return myArray;
+  }
+  
+  function reduceToTotal(sourceArray, startingPoint=0){
+    for (let item of sourceArray) {
+      startingPoint = startingPoint + item;
+    }
+    return startingPoint;
+  }
+  
+  function reduceToAllTrue(sourceArray){
+    for (let item of sourceArray) {
+      if(item == false){
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  function reduceToAnyTrue(sourceArray){
+    for (let item of sourceArray) {
+      if(item == true){
+        return true;
+      }
+    }
+    return false;
+  }
